8c75ca6b5d85a1446f452f3d981f8f565b2a54b2
Hélio Barolo <helio.barolo@novahaus.com.br>
2023-02-02 12:17:04 -0300

    Desenvolvimento

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '../assets/logo.svg'
+
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header>
+        <img src={logoImg} alt="logo ignite" />
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
