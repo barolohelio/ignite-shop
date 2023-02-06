55f86084ca673b2ff7d2e3160542061baf158f16
Hélio Barolo <helio.barolo@novahaus.com.br>
2023-02-06 12:02:48 -0300

    Estrutura visual - Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,20 @@ import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
 import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app';
+
+import Image from 'next/image';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>
-        <img src={logoImg} alt="logo ignite" />
-      </header>
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo ignite" />
+      </Header>
 
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  color: '#fff',
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
+
 
 export default function Home() {
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt={""} />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt={""} />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
   )
 }
